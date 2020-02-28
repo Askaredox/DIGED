905433d450bded2ae07bb25da70698eff83e4301
Askaredox <andyecarvajal@gmail.com>
2020-02-27 19:26:21 -0600

    v0.2.0[Admin_page elab]

---
+++ application/controllers/cLogin.php
@@ -0,0 +1,42 @@
+<?php
+class CLogin extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+
+        // Load form helper library
+        $this->load->helper('form');
+
+        // load Session Library
+        $this->load->library('session');
+
+        // load url helper
+        $this->load->helper('url');
+    }
+    public function index(){
+        $this->load->view('vLogin');
+    }
+    public function user_login_process(){
+        $data = array(
+            'user_name' => $this->input->get('txtUser'),
+            'user_pass' => $this->input->get('txtPass')
+        );
+        
+        if($data['user_name']=="admin" && $data['user_pass']=="admin"){
+            $this->session->set_userdata('logged_in', $data);
+            $this->load->view('vAdmin_page');
+            /*
+            echo '<pre>';
+            print_r($this->session->userdata());
+            echo '</pre>';
+            */
+        }
+        else{
+            $this->session->sess_destroy();
+            $this->load->view('vLogin');
+            echo '<pre>';
+            print_r($this->session->userdata());
+            echo '</pre>';
+        }
+        
+    }
+}
