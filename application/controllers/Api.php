5f63fe10cd544e66358212cff2708a5852789a2e
Askaredox <andyecarvajal@gmail.com>
2020-04-29 23:12:15 -0600

    problema de CORS

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -2,11 +2,13 @@
    
 require APPPATH . 'libraries/REST_Controller.php';
 class Api extends REST_Controller{
-
+    protected $allowed_http_methods = array('get', 'delete', 'post', 'put');
     public function __construct() {
         parent::__construct();
         $this->load->model(array('Api_model'));
         $this->load->database();
+
+        
     }
     /**
      * http://localhost/DEDEV/Api/cursos
@@ -14,6 +16,13 @@ class Api extends REST_Controller{
      */
     public function cursos_get()
 	{
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == "OPTIONS") {
+            die();
+        }
         $data=$this->Api_model->getCursos();
 
         $this->response($data, REST_Controller::HTTP_OK);
@@ -23,6 +32,13 @@ class Api extends REST_Controller{
      * https://desarrollo.virtual.usac.edu.gt/DIGED/Api/temas/4
      */
     public function temas_get($id){
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == "OPTIONS") {
+            die();
+        }
         if(!is_numeric($id)){
             $this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
             return;
@@ -39,6 +55,13 @@ class Api extends REST_Controller{
      * https://desarrollo.virtual.usac.edu.gt/DIGED/Api/img/2
     */
     public function img_get($id){
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == "OPTIONS") {
+            die();
+        }
         $data1=$this->Api_model->getTemaImg($id);
         $data2=$this->Api_model->getTituloCords($id);
 
@@ -61,6 +84,13 @@ class Api extends REST_Controller{
      * https://desarrollo.virtual.usac.edu.gt/DIGED/Api/titulo/2
     */
     public function titulo_get($id){
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        $method = $_SERVER['REQUEST_METHOD'];
+        if($method == "OPTIONS") {
+            die();
+        }
         $data=$this->Api_model->getContenido($id);
         if($data)
             $this->response($data, REST_Controller::HTTP_OK);
